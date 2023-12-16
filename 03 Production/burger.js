599bacbb6bb9be1bfa1a8db920b8610db77147bf
Dmytro Bodnar <132192473+Dmytro0812@users.noreply.github.com>
2023-12-16 23:12:51 +0100

    Delete 03 Production directory

--- 03 Production/burger.js
+++
@@ -1,8 +0,0 @@
-const burger = document.getElementById("burger")
-const NavList = document.getElementById("nav__list")
-
-
-burger.onclick = function() {
-    NavList.classList.toggle("open")
-    burger.classList.toggle("b_transition")
-}
